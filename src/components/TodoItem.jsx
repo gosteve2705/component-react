85094307fed3a7d83b2b58e88933396cb6506aea
Nomuoja Oghenemaro <38343496+gosteve2705@users.noreply.github.com>
2020-07-19 11:40:17 +0100

    update  todoitem to handle clicks

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TodoItem(props){
-return <li> {props.item}</li>
+function TodoItem(props) {
+  const { isClicked, setisClicked } = useState(false);
+  function handleClick() {
+    setisClicked(prevValue => {
+      return !prevValue;
+    });
+  }
+  return (
+    <li
+      onClick={handleClick}
+      style={{ textDecoration: isClicked ? "line-through" : "none" }}
+    >
+      {props.item}
+    </li>
+  );
 }
 
 export default TodoItem;
